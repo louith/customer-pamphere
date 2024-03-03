8069f32957891d7bd284dc4094dacf4f8134b903
jasperlawrencee <caballerojasper.55555@gmail.com>
2024-03-03 11:01:06 +0800

    Squashed commit of the following:
    commit 2b5227a404e7681d7732c5189a395f22b22ca77c
    Author: jasperlawrencee <caballerojasper.55555@gmail.com>
    Date:   Fri Mar 1 15:50:19 2024 +0800
    
        can send image to chat
    
    commit 9387e85adc4be490b6eb82e300f9607015c3df44
    Author: jasperlawrencee <caballerojasper.55555@gmail.com>
    Date:   Wed Feb 28 11:38:34 2024 +0800
    
        can add images to chat partial
    
    commit 736f47d83c5e3548c7f0054206f0513065d5180e
    Author: jasperlawrencee <caballerojasper.55555@gmail.com>
    Date:   Tue Feb 27 21:18:24 2024 +0800
    
        finished text chat

--- customer/lib/screens/Chat/chatBubble.dart
+++ customer/lib/screens/Chat/chatBubble.dart
@@ -8,16 +8,32 @@ class ChatBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(12),
-      margin: const EdgeInsets.only(bottom: 8),
-      decoration: const BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        color: kPrimaryLightColor,
-      ),
-      child: Text(
-        message,
-        style: const TextStyle(fontSize: 16),
-      ),
-    );
+        padding: const EdgeInsets.all(12),
+        margin: const EdgeInsets.only(bottom: 8),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: kPrimaryLightColor,
+        ),
+        child: message.substring(0, 4) != "http"
+            ? Text(
+                message,
+                style: const TextStyle(fontSize: 16),
+              )
+            : InkWell(
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        content: Image.network(message),
+                      );
+                    },
+                  );
+                },
+                child: Image.network(
+                  message,
+                  height: 100,
+                ),
+              ));
   }
 }
