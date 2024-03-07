f7981c5a8cace6a657c319d3425411cac08c3912
louith <sepradoloys@gmail.com>
2024-03-07 09:33:58 +0800

    forda services (incomplete)

--- customer/lib/screens/Booking/bookingScreen.dart
+++ customer/lib/screens/Booking/bookingScreen.dart
@@ -1,5 +1,6 @@
 import 'package:customer/components/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class BookingScreen extends StatefulWidget {
   const BookingScreen({super.key});
@@ -12,16 +13,19 @@ class _BookingScreenState extends State<BookingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kPrimaryColor,
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(Icons.arrow_back_ios)),
-        title: Text('Booking indiv worker'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: kPrimaryColor,
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios)),
+          title: Text('Booking indiv worker'),
+        ),
+        body: Container(
+            child: SfCalendar(
+          view: CalendarView.month,
+        )));
     ;
   }
 }
